dec3566f0c4c3f70487b6440c98076a2ba67350d
Dante <dalvarezt@unjbg.edu.pe>
2024-06-20 23:37:37 -0500

    Se agrego el ejercicio 1 de funciones II: El ejercicio trata sobre hallar el area de un circulo. Se debe usar funciones y la libreria cmath

---
+++ ejercicio1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//Función para hallar el área del circulo
+double areaCirculo( double x ) {
+	double y = M_PI * pow( x, 2 );
+	return y;
+}
+
+int main() {
+	double radio, area;
+	cout << "Ingrese el radio del circulo para calcular el area: ";
+	cin >> radio;
+	area = areaCirculo( radio );
+	cout << "El area del circulo es:" << area;
+}
