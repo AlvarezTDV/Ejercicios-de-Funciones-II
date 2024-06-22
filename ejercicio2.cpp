2d8a206e01df63449fa325a069aae4c953e028aa
Dante <dalvarezt@unjbg.edu.pe>
2024-06-21 22:42:30 -0500

    Se agrego el ejercicio 2 de funciones II: El ejercicio trata sobre que el usuario debe ingresar una cantidad de numeros aleatorios que se va a generar, y tambien ingresar el número máximo que pueden llegar los números aleatorios

---
+++ ejercicio2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int generarNumAleatorio( int x ) {
+	int y = rand() % ( x + 1 );
+	return y;
+}
+
+int main() {
+	int numero, numMax, numAleatorio;
+	cout << "Ingrese la cantidad de numeros que desea generar: ";
+	cin >> numero;
+	cout << "Ingrese el valor maximo de los numeros aleatorios: ";
+	cin >> numMax;
+	for ( int i = 1; i <= numero; i++ ) {
+		numAleatorio = generarNumAleatorio( numMax );
+		cout << numAleatorio << endl;
+	}
+	return 0;
+}
