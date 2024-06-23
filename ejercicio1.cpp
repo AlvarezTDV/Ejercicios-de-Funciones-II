e511933d1df6ecd1937a629b6034be5338fa60bf
Dante <dalvarezt@unjbg.edu.pe>
2024-06-22 23:56:33 -0500

    Mejora al ejercicio 1 de funciones II: Se añadio una condicion que verifica si el radio ingresado es positivo, de lo contrario mostraria un mensaje de número inválido

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 
-//Función para hallar el área del circulo
+//Funcion para hallar el Area del circulo
 double areaCirculo( double x ) {
 	double y = M_PI * pow( x, 2 );
 	return y;
@@ -12,7 +12,11 @@ int main() {
 	double radio, area;
 	cout << "Ingrese el radio del circulo para calcular el area: ";
 	cin >> radio;
-	area = areaCirculo( radio );
-	cout << "El area del circulo es:" << area;
+	if ( radio >= 0 ) {
+		area = areaCirculo( radio );
+		cout << "El area del circulo es:" << area;
+	} else {
+		cout << "ERROR: Ingreso un numero invalido";
+	}
 	return 0;
 }
